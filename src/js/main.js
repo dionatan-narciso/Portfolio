466813977619eb36482e2fc8f36065a2fce4b5c8
dionatan-narciso <dionatan.narciso@gmail.com>
2023-05-04 12:25:23 +1200

    updated email section

--- src/js/main.js
+++ src/js/main.js
@@ -1,120 +1,119 @@
 // -----------SHOW MENU----------- //
-const navMenu = document.getElementById("nav-menu"),
-  navToggle = document.getElementById("nav-toggle"),
-  navClose = document.getElementById("nav-close");
+const navMenu = document.getElementById('nav-menu'),
+  navToggle = document.getElementById('nav-toggle'),
+  navClose = document.getElementById('nav-close')
 
 // Menu Show //
 // Validate if constant exist //
 if (navToggle) {
-  navToggle.addEventListener("click", () => {
-    navMenu.classList.add("show-menu");
-  });
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu')
+  })
 }
 // Menu Hidden //
 if (navClose) {
-  navClose.addEventListener("click", () => {
-    navMenu.classList.remove("show-menu");
-  });
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu')
+  })
 }
 
 // Remove Menu Mobile //
-const navLink = document.querySelectorAll(".nav__link");
+const navLink = document.querySelectorAll('.nav__link')
 
 const linkAction = () => {
-  const navMenu = document.getElementById("nav-menu");
+  const navMenu = document.getElementById('nav-menu')
 
-  navMenu.classList.remove("show-menu");
-};
-navLink.forEach((el) => el.addEventListener("click", linkAction));
+  navMenu.classList.remove('show-menu')
+}
+navLink.forEach((el) => el.addEventListener('click', linkAction))
 
 // Add Blur to Header //
 const blurHeader = () => {
-  const header = document.getElementById("header");
+  const header = document.getElementById('header')
   this.scrollY >= 50
-    ? header.classList.add("blur-header")
-    : header.classList.remove("blur-header");
-};
-window.addEventListener("scroll", blurHeader);
+    ? header.classList.add('blur-header')
+    : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
 
 // Email JS //
-const contactForm = document.getElementById("contact-form"),
-  contactMessage = document.getElementById("contact-message");
+const contactForm = document.getElementById('contact-form'),
+  contactMessage = document.getElementById('contact-message')
 
 const sendEmail = (e) => {
-  e.preventDefault();
+  e.preventDefault()
 
   emailjs
     .sendForm(
-      "service_89j8ndv",
-      "template_o450b1s",
-      "#contact-form",
-      "B2n_U6rKZ5vIhg3Zf"
+      'service_pi6khye',
+      'template_wqz29r3',
+      '#contact-form',
+      'vl323Fjgd8I9y1Yi0'
     )
     .then(
       () => {
-        contactMessage.textContent = "Message sent successfully ✅";
+        contactMessage.textContent = 'Message sent successfully ✅'
 
         setTimeout(() => {
-          contactMessage.textContent = "";
-        }, 5000);
+          contactMessage.textContent = ''
+        }, 5000)
 
-        contactForm.reset();
+        contactForm.reset()
       },
       () => {
-        contactMessage.textContent = "Message not sent (service error) ❌";
+        contactMessage.textContent = 'Message not sent (service error) ❌'
       }
-    );
-};
+    )
+}
 
 // Scroll Up Button Display
 const scrollUp = () => {
-  const scrollUp = document.getElementById("scroll-up");
+  const scrollUp = document.getElementById('scroll-up')
   this.scrollY >= 350
-    ? scrollUp.classList.add("show-scroll")
-    : scrollUp.classList.remove("show-scroll");
-};
+    ? scrollUp.classList.add('show-scroll')
+    : scrollUp.classList.remove('show-scroll')
+}
 
-window.addEventListener("scroll", scrollUp);
+window.addEventListener('scroll', scrollUp)
 
-contactForm.addEventListener("submit", sendEmail);
+contactForm.addEventListener('submit', sendEmail)
 
 // Scrol Section Active Link
-const sections = document.querySelectorAll("section[id]");
+const sections = document.querySelectorAll('section[id]')
 
 const scrollActive = () => {
-  const scrollY = window.pageYOffset;
+  const scrollY = window.pageYOffset
 
   sections.forEach((current) => {
     const sectionHeight = current.offsetHeight,
       sectionTop = current.offsetTop - 58,
-      sectionId = current.getAttribute("id"),
+      sectionId = current.getAttribute('id'),
       sectionsClass = document.querySelector(
-        ".nav__menu a[href*=" + sectionId + "]"
-      );
+        '.nav__menu a[href*=' + sectionId + ']'
+      )
 
     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-      sectionsClass.classList.add("active-link");
+      sectionsClass.classList.add('active-link')
     } else {
-      sectionsClass.classList.remove("active-link");
+      sectionsClass.classList.remove('active-link')
     }
-  });
-};
-window.addEventListener("scroll", scrollActive);
+  })
+}
+window.addEventListener('scroll', scrollActive)
 
 // Scroll Reveal Animation
 const sr = ScrollReveal({
-  origin: "top",
-  distance: "60px",
+  origin: 'top',
+  distance: '60px',
   duration: 2500,
   delay: 400,
   // reset: true //Animations repeat
-});
+})
 
 // Projects Description
 const projectsDesc = document.querySelectorAll('.projects__desc')
 const projectBtn = document.querySelectorAll('.project__desc-btn')
-const originalDesc = [];
-
+const originalDesc = []
 
 projectsDesc.forEach((desc) => {
   originalDesc.push(desc.innerHTML)
@@ -123,21 +122,18 @@ projectsDesc.forEach((desc) => {
 
 projectBtn.forEach((btn, i) => {
   btn.addEventListener('click', () => {
-    if(btn.textContent === 'show more') {
+    if (btn.textContent === 'show more') {
       projectsDesc[i].innerHTML = originalDesc[i]
       btn.innerHTML = 'show less'
-    }  else {
+    } else {
       projectsDesc[i].innerHTML = `${originalDesc[i].slice(17, 57)}.....`
       btn.innerHTML = 'show more'
     }
   })
 })
 
-
-sr.reveal(`.home__data, .home__social, .contact__container, .footer__container`);
-sr.reveal(`.home__image`, { origin: "bottom" });
-sr.reveal(`.about__data, .skills__data`, { origin: "left" });
-sr.reveal(`.about__image, .skills__content`, { origin: "right" });
-sr.reveal(`.services__card, .projects__card-container`, { interval: 100 });
-
-
+sr.reveal(`.home__data, .home__social, .contact__container, .footer__container`)
+sr.reveal(`.home__image`, { origin: 'bottom' })
+sr.reveal(`.about__data, .skills__data`, { origin: 'left' })
+sr.reveal(`.about__image, .skills__content`, { origin: 'right' })
+sr.reveal(`.services__card, .projects__card-container`, { interval: 100 })
